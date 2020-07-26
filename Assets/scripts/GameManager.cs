86ddf560766b70667d44a738c889f592b8008497
S.Pragashraj <rajpragashraj30@gmail.com>
2020-07-26 15:49:49 +0530

    game starter screen

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+    private PlayerHealth playerHealth;
+    public GameObject gameOver;
+    private playerMovement PlayerMovement;
+    public GameObject pause;
+
+    void Start()
+    {
+        playerHealth = player.GetComponent<PlayerHealth>();
+        PlayerMovement = player.GetComponent<playerMovement>();
+    }
+
+    void Update()
+    {
+        if (playerHealth.health <= 0)
+        {
+            gameOver.SetActive(true);
+            PlayerMovement.enabled = false;
+            Time.timeScale = 0;
+        }
+    }
+
+
+    public void pauseGame()
+    {
+        pause.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+
+    public void resume()
+    {
+        pause.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
---
+++ Assets/scripts/starter/Starter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Starter : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void resume()
+    {
+        pauseMenu.SetActive(false);
+    }
+}
